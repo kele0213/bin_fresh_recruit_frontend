825cfeb0d97440fb8b264caa02592ea2e27d030d
xiaobinhong <xiaobinhong@xiaoe-tech.com>
2024-04-04 07:34:37 +0800

    feat: 使用本地后端

--- src/global/setupMenu.ts
+++ src/global/setupMenu.ts
@@ -3,7 +3,6 @@ import { useAuthStore } from '@/stores/user/auth'
 export default async function setupMenu() {
   const auth = useAuthStore()
   const { role, registerRouter } = auth
-  console.log(role)
   if (role == 'school' || role == 'company') {
     await registerRouter(role, 'main')
   } else if (role == 'fresh') {
